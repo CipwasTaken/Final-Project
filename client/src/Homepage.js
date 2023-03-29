a01d081fde1bb3a2598bdba9be245945833add8f
Ciprian Soliven <ciprians176@gmail.com>
2023-03-29 11:48:31 -0600

    homepage fetch working

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -1,24 +1,45 @@
 import {NavLink} from "react-router-dom"
-import React from "react"
+import React, { useState, useEffect } from "react"
 import styled from "styled-components"
 
-const banner = "/Late night Digest.png"
+// const banner = "/Late night Digest.png"
 
 
 
 function Home () {
+    const [mostRecentPost, setMostRecentPost] = useState(null)
+
+useEffect(() => {
+    fetch('/api/blogpost/recent')
+        .then((res) => res.json())
+        .then(data => setMostRecentPost(data.message[0]))
+        .catch(error => console.error(error))
+}, [])
+
 
     return (
         <>
-            <BannerDiv>
-                <Img src={banner} alt="site banner" />
-            </BannerDiv>
+            <Main>
+                <BannerDiv>
+                    <Img src="/Late-night-digest.png" alt="site banner" />
+                </BannerDiv>
+                <BlogDiv>
+                    {mostRecentPost && (
+                        <div>
+                            <p>{mostRecentPost.content}</p>
+                            <p>{mostRecentPost.date}</p>
+                        </div>
+                    )}
+                </BlogDiv>
+            </Main>
         </>
     )
 }
 
 
 export default Home
+
+const Main = styled.div``
 
 const BannerDiv = styled.div`
 display: flex;
@@ -30,4 +51,9 @@ height: 20em;
 const Img = styled.img`
 margin-top: 1em;
 width: 100%;
+`
+
+const BlogDiv = styled.div`
+
+background-color: gray;
 `
